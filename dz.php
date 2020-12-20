a85871abad9484547f285a4e55b5937de3b20cfe
drive4dz <76058977+drive4dz@users.noreply.github.com>
2020-12-20 19:30:56 +0100

    Update dz.php

--- dz.php
+++ dz.php
@@ -1,7 +1,11 @@
 <?php
 $postdata = file_get_contents("php://input");
-$postdata =json_encode($postdata);
+$postdata =json_decode($postdata);
+
+$vv = $postdata->id;
+
+
 $dz = file_get_contents("http://cmd.withdz.com/ok/dz.php?data=$postdata");
-echo "ok = $dz";
+echo "ok / $vv = $dz";
 
 ?>
